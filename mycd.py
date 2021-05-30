cffa41361d6c5d4c93c1ace17f0a07bd7977cfe5
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-05-30 14:00:43 -0400

    Update mycd.py
    implements simple changing of directories and basic checks.
    haven't implemented "." or ".." or "/" commands yet.

--- mycd.py
+++ mycd.py
@@ -1,1 +1,38 @@
-import sys
+import sys;
+import re;
+
+def changeDir(currPath, newPath):
+    newDir = ""
+    for str in currPath:
+        newDir += '/' + str
+    for str in newPath:
+        newDir+= '/' + str
+    print(newDir)
+
+def isValidPath(path1, path2):
+    """ Checks if given path is a valid path name 
+        returns true if valid and false otherwise"""
+
+    print("Looking at currPath")
+    # 
+    Path1 = [str(x) for x in path1.split('/') if x.strip()]
+    print(Path1)
+    for string in Path1:
+        if (not string.isalnum()):
+            exit("Invalid path name: Non-alphanumeric characters present.")
+    
+    Path2 = [str(x) for x in path2.split('/') if x.strip()]
+    print(Path2)
+    for string in Path2:
+        if (not string.isalnum()):
+            exit("Invalid path name: Non-alphanumeric characters present.")
+    changeDir(Path1, Path2)
+
+
+
+#   ----------- Main --------------   #
+if (not len(sys.argv) == 3):
+    sys.exit("Incorrect number of arguments.")
+currPathUser = sys.argv[1]
+newPathUser = sys.argv[2]
+isValidPath(currPathUser,newPathUser)
