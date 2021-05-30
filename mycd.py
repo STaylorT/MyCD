204adf9735ddd58a1d331173530b2b859fa9bec0
STaylorT <51551018+STaylorT@users.noreply.github.com>
2021-05-30 16:35:28 -0400

    Update mycd.py
    "..", "/"  cases are working well
    few minor bugs

--- mycd.py
+++ mycd.py
@@ -1,38 +1,58 @@
 import sys;
 import re;
 
-def changeDir(currPath, newPath):
-    newDir = ""
-    for str in currPath:
-        newDir += '/' + str
-    for str in newPath:
-        newDir+= '/' + str
-    print(newDir)
-
-def isValidPath(path1, path2):
-    """ Checks if given path is a valid path name 
-        returns true if valid and false otherwise"""
-
-    print("Looking at currPath")
-    # 
-    Path1 = [str(x) for x in path1.split('/') if x.strip()]
-    print(Path1)
+def Parser(path1, path2):
+    """ Checks if both paths are valid and performs correct 
+        directory change based on second path name"""
+    # check if current directory is valid
+    if (len(path1) > 0 and path1[0][:1] != '/'):
+            exit('Current directory invalid')
+    # list to keep track of path names to add from path2
+    newPath = []
+    # split current directory into directory/filename tokens
+    Path1 = [x for x in path1.split('/') if x.strip()]
     for string in Path1:
         if (not string.isalnum()):
-            exit("Invalid path name: Non-alphanumeric characters present.")
-    
-    Path2 = [str(x) for x in path2.split('/') if x.strip()]
-    print(Path2)
-    for string in Path2:
-        if (not string.isalnum()):
-            exit("Invalid path name: Non-alphanumeric characters present.")
-    changeDir(Path1, Path2)
-
-
+            exit("Invalid current path: Non-alphanumeric characters present.")
+    # if path2 starts with "/", return to home directory
+        if (len(path2) > 0 and path2[0] == "/"):
+             for char in path2:
+                 if (char != '/'):
+                     exit("Invalid Command")
+        exit("/")
+     # split current directory into directory/filename tokens
+    Path2 = [x for x in path2.split('/') if x.strip()]   
+    # iterate over the tokens in path2 to parse command     
+    for tok in Path2:
+        if(tok == "."):
+            continue
+        elif(tok == ".."):
+            if (len(newPath) < 1):
+                if (len(Path1) > 0):
+                    Path1.pop()
+            else:
+                newPath.pop()
+        elif(tok.isalnum()):
+            newPath.append(tok)
+        else:
+            exit("No such file or directory")
+    # create new pathname         
+    retPath = []
+    for str1 in Path1:
+        retPath += '/' + str1
+    for str1 in newPath:
+        retPath += '/' + str1
+    retStr = ""
+    for char in retPath:
+        retStr += char
+    if (len(retStr) == 0):
+        print ("/")
+    else:
+        print (retStr)
 
 #   ----------- Main --------------   #
 if (not len(sys.argv) == 3):
     sys.exit("Incorrect number of arguments.")
 currPathUser = sys.argv[1]
 newPathUser = sys.argv[2]
-isValidPath(currPathUser,newPathUser)
+Parser(currPathUser,newPathUser)
